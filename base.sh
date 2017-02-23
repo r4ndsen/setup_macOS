e48478a02030b904654a87a1b1e142f8840e2f07
Werner Spiegel <ws@products-up.de>
2017-02-23 09:00:01 +0100

    Update base.sh

--- base.sh
+++ base.sh
@@ -2,3 +2,7 @@
 defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
 
+brew install curl --with-libssh2
+brew link curl --force
+
+brew install php71 --with-homebrew-curl
