7c4ab95af6399ee71cda790676722547c2b975a9
Werner Spiegel <ws@products-up.de>
2017-02-23 10:08:18 +0100

    Update base.sh

--- base.sh
+++ base.sh
@@ -2,7 +2,20 @@
 defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
 
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install wget
 brew install curl --with-libssh2
 brew link curl --force
 
 brew install php71 --with-homebrew-curl
+brew install php71-redis
+brew install php71-pspell
+brew install php71-pcntl
+brew install php71-intl
+#brew install php71-ssh2
+
+brew install composer
+brew install redis
+brew install htop
+brew install python
