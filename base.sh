12cf0f003c83997dd25d6e065ebbccc3e5a0b20a
Werner Spiegel <ws@products-up.de>
2017-06-13 09:45:47 +0200

    Create base.sh

--- base.sh
+++ base.sh
@@ -12,6 +12,7 @@ brew install php71 --with-homebrew-curl
 brew install php71-redis
 brew install php71-pspell
 brew install php71-propro
+brew install php71-raphf
 #brew install php71-imagick
 brew install php71-pcntl
 brew install php71-intl
