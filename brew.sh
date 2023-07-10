77a89219f9b343447e14ef06da19f1d1e0ad6c53
Werner Spiegel <werner.spiegel@gmail.com>
2023-07-10 13:22:50 +0200

    Update and rename base.sh to brew.sh

---
+++ brew.sh
@@ -0,0 +1,22 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install \
+	pv \
+	composer \
+	htop \
+	moreutils \
+	php \
+  go \
+  jq \
+	colima \
+	htop \
+	automake \
+  autoconf \
+  zstd \
+  python \
+	gh \
+  moom	\
+  sequel-pro \ 
+  sublime-text \	
+  vox \
+  warp
