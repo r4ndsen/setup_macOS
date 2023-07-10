c62aa02b0bd09ad7c6ed5bba228b0735a8dd30a1
Werner Spiegel <werner.spiegel@gmail.com>
2023-07-10 13:23:32 +0200

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -1,22 +1,22 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew install \
-	pv \
-	composer \
-	htop \
-	moreutils \
-	php \
+  pv \
+  composer \
+  htop \
+  moreutils \
+  php \
   go \
   jq \
-	colima \
-	htop \
-	automake \
+  colima \
+  htop \
+  automake \
   autoconf \
   zstd \
   python \
-	gh \
-  moom	\
+  gh \
+  moom \
   sequel-pro \ 
-  sublime-text \	
+  sublime-text \
   vox \
   warp
